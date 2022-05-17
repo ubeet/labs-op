5877559111518476d037d9667121098efa9ba7db
Владислав Громов <44301460+ubeet@users.noreply.github.com>
2022-05-17 13:27:51 +0300

    Delete Лаба1 directory

--- Лаба1/лаба1.py
+++
@@ -1,5 +0,0 @@
-import math
-h = int(input("Введите высоту: "))
-r = int(input("Введите радиус: "))
-print("Объем цилиндра: ", round(math.pi * math.pow(r, 2) * h, 3))
-print("Объем конуса: ", round(1/3 * math.pi * math.pow(r, 2) * h, 3))
